a99f07f986556937cf3b2a584ecca248789a3b38
azeezabidoye <azeezabidoye@yahoo.com>
2022-05-12 16:22:49 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Create a grid-of-two containing a form and card to display
+ * The card is hidden on page-load until user submits data through the form
+ * The card then displays the user's information on it
+ * 
+ * FORM
+ * First name
+ * Last name
+ * Account type (savings, current, business)
+ * 
+ * ATM CARD Displays:
+ * Account type
+ * 16-digit randomly generated number
+ * NB: Only the last four-digits will be displayed
+ * First is capilized
+ * Last name in uppercase
+ * Expiry date (05/25) current year plus 3!
+ */
